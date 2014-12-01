9007d4a6ebc4ef6d112a502f26c0ec3811585720
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2014-12-01 19:12:42 +0100

    Added very basic templates for displaying fleets and players

--- handlers.go
+++ handlers.go
@@ -2,15 +2,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/morpheusxaut/lootsheeter/models"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+	}{}
 
+	templates.ExecuteTemplate(w, "index", data)
 }
 
 func TrustRequestHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,26 +21,24 @@ func TrustRequestHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func FleetListHandler(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Fleets []*models.Fleet
+	}{}
+
 	fleets, err := database.LoadAllFleets()
 	if err != nil {
 		logger.Errorf("Failed to load all fleets in FleetListHandler: [%v]", err)
 
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	data.Fleets = fleets
 
-	fmt.Fprintln(w, "<html><body>")
-
-	for _, fleet := range fleets {
-		fmt.Fprintf(w, "ID: <a href=\"fleet/%d\">%d</a>, Name: %s, System: %s, Sites Finished: %d, Start: %v, End: %v<br />", fleet.Id, fleet.Id, fleet.Name, fleet.System, fleet.SitesFinished, fleet.StartTime, fleet.EndTime)
-		for _, member := range fleet.Members {
-			fmt.Fprintf(w, "Fleet #%d Member - ID: %d, Name: %s, Role: %s<br />", fleet.Id, member.Id, member.Player.Name, member.Role)
-		}
+	err = templates.ExecuteTemplate(w, "fleets", data)
+	if err != nil {
+		logger.Errorf("Failed to execute template in FleetListHandler: [%v]", err)
 	}
-
-	fmt.Fprintln(w, "</body></html>")
 }
 
 func FleetCreateHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,23 +51,27 @@ func FleetDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		logger.Errorf("Failed to parse fleet ID %q in FleetDetailsHandler: [%v]", vars["fleetid"], err)
 
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	data := struct {
+		Fleet *models.Fleet
+	}{}
 
 	fleet, err := database.LoadFleet(fleetId)
 	if err != nil {
 		logger.Errorf("Failed to load details for fleet #%d in FleetDetailsHandler: [%v]", fleetId, err)
 
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	data.Fleet = fleet
 
-	fmt.Fprintf(w, "ID: %d, Name: %s, System: %s, System Nickname: %s, Profit: %f, Losses: %f, Sites Finished: %d, Start: %v, End: %v\n", fleet.Id, fleet.Name, fleet.System, fleet.SystemNickname, fleet.Profit, fleet.Losses, fleet.SitesFinished, fleet.StartTime, fleet.EndTime)
-	for _, member := range fleet.Members {
-		fmt.Fprintf(w, "Fleet #%d Member - ID: %d, Name: %s, Player ID: %d, Site Modifier: %d, Payment Modifier: %f, Role: %s\n", fleet.Id, member.Id, member.Player.Name, member.Player.PlayerId, member.SiteModifier, member.PaymentModifier, member.Role)
+	err = templates.ExecuteTemplate(w, "fleetdetails", data)
+	if err != nil {
+		logger.Errorf("Failed to execute template in FleetDetailsHandler: [%v]", err)
 	}
 }
 
@@ -79,18 +84,23 @@ func FleetDeleteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PlayerListHandler(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Players []*models.Player
+	}{}
+
 	players, err := database.LoadAllPlayers()
 	if err != nil {
 		logger.Errorf("Failed to load all players in PlayerListHandler: [%v]", err)
 
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	data.Players = players
 
-	for _, player := range players {
-		fmt.Fprintf(w, "ID: %d, Name: %s, Corporation: %s\n", player.Id, player.Name, player.Corporation.Name)
+	err = templates.ExecuteTemplate(w, "players", data)
+	if err != nil {
+		logger.Errorf("Failed to execute template in PlayerListHandler: [%v]", err)
 	}
 }
 
@@ -104,21 +114,28 @@ func PlayerDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		logger.Errorf("Failed to parse player ID %q in PlayerDetailsHandler: [%v]", vars["playerid"], err)
 
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	data := struct {
+		Player *models.Player
+	}{}
 
 	player, err := database.LoadPlayer(playerId)
 	if err != nil {
 		logger.Errorf("Failed to load details for player #%d in PlayerDetailsHandler: [%v]", playerId, err)
 
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	data.Player = player
 
-	fmt.Fprintf(w, "ID: %d, Name: %s, Player ID: %d, Corporation: %s, Access: %s\n", player.Id, player.Name, player.PlayerId, player.Corporation.Name, player.AccessMask)
+	err = templates.ExecuteTemplate(w, "playerdetails", data)
+	if err != nil {
+		logger.Errorf("Failed to execute template in PlayerDetailsHandler: [%v]", err)
+	}
 }
 
 func PlayerEditHandler(w http.ResponseWriter, r *http.Request) {
