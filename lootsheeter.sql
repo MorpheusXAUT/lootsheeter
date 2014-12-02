4a8aa3c66b4516251099ebc41e863874e3829ff6
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2014-12-02 16:31:46 +0100

    Updated database to use payment modifier of 1 as default

--- lootsheeter.sql
+++ lootsheeter.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         network.morpheusxaut.net
--- Server version:               5.5.40-0ubuntu0.14.04.1 - (Ubuntu)
--- Server OS:                    debian-linux-gnu
--- HeidiSQL Version:             9.1.0.4880
+-- Host:                         127.0.0.1
+-- Server version:               5.6.19-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.1.0.4882
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -11,7 +11,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping database structure for lootsheeter
-CREATE DATABASE IF NOT EXISTS `lootsheeter` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+CREATE DATABASE IF NOT EXISTS `lootsheeter` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `lootsheeter`;
 
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `fleetmembers` (
   `player_id` bigint(20) NOT NULL,
   `role` int(10) NOT NULL DEFAULT '0',
   `site_modifier` int(10) NOT NULL DEFAULT '0',
-  `payment_modifier` double NOT NULL DEFAULT '0',
+  `payment_modifier` double NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `fleet_id_player_id` (`fleet_id`,`player_id`),
   KEY `fk_fleetmembers_player` (`player_id`),
