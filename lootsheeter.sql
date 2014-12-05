a22cec60a618cd221083a6029bdf8ae62a5285a6
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2014-12-05 20:17:27 +0100

    Updated SQL setup for database Fixed error in reports overview template

--- lootsheeter.sql
+++ lootsheeter.sql
@@ -40,12 +40,15 @@ CREATE TABLE IF NOT EXISTS `fleetmembers` (
   `payment_modifier` double NOT NULL DEFAULT '1',
   `payout` double NOT NULL DEFAULT '0',
   `payout_complete` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
+  `report_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `fleet_id_player_id` (`fleet_id`,`player_id`),
   KEY `fk_fleetmembers_player` (`player_id`),
   KEY `fk_fleetmembers_fleet` (`fleet_id`),
+  KEY `fk_fleetmembers_report` (`report_id`),
   CONSTRAINT `fk_fleetmembers_fleet` FOREIGN KEY (`fleet_id`) REFERENCES `fleets` (`id`),
-  CONSTRAINT `fk_fleetmembers_player` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
+  CONSTRAINT `fk_fleetmembers_player` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`),
+  CONSTRAINT `fk_fleetmembers_report` FOREIGN KEY (`report_id`) REFERENCES `reports` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Data exporting was unselected.
@@ -62,9 +65,13 @@ CREATE TABLE IF NOT EXISTS `fleets` (
   `sites_finished` int(10) NOT NULL DEFAULT '0',
   `start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `end` timestamp NULL DEFAULT NULL,
+  `corporation_payout` double NOT NULL DEFAULT '0',
   `payout_complete` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
+  `report_id` bigint(20) DEFAULT NULL,
   `active` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Y',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_fleets_report` (`report_id`),
+  CONSTRAINT `fk_fleets_report` FOREIGN KEY (`report_id`) REFERENCES `reports` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Data exporting was unselected.
@@ -84,6 +91,22 @@ CREATE TABLE IF NOT EXISTS `players` (
   KEY `fk_players_corporation` (`corporation_id`),
   CONSTRAINT `fk_players_corporation` FOREIGN KEY (`corporation_id`) REFERENCES `corporations` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table lootsheeter.reports
+CREATE TABLE IF NOT EXISTS `reports` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_by` bigint(20) NOT NULL,
+  `total_payout` double NOT NULL DEFAULT '0',
+  `startrange` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `endrange` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `payout_complete` enum('Y','N') NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`id`),
+  KEY `fk_reports_player` (`created_by`),
+  CONSTRAINT `fk_reports_player` FOREIGN KEY (`created_by`) REFERENCES `players` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
