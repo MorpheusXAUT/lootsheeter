a35a0b9d3d85f4212f04e28d191644eefa0e0530
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2014-12-09 23:55:50 +0100

    Started general reformatting and cleaning of the fleet details template Reduced own javascript to simple toggle function and a click-listener for the collapse-buttons Continued preparations for the AJAX handler

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,33 +1,11 @@
-function toggleFleetmemberEdit(index, enable) {
-	if (enable) {
-		$('#form_fleetmember_role_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_role_edit_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_sitemodifier_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_sitemodifier_edit_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_paymentmodifier_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_paymentmodifier_edit_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_payout_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_payout_edit_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_payoutcomplete_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_payoutcomplete_edit_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_action_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_action_edit_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_action_cancel_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_action_cancel_edit_'.concat(index)).removeClass('hide');
-	} else {
-		$('#form_fleetmember_role_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_role_edit_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_sitemodifier_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_sitemodifier_edit_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_paymentmodifier_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_paymentmodifier_edit_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_payout_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_payout_edit_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_payoutcomplete_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_payoutcomplete_edit_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_action_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_action_edit_'.concat(index)).addClass('hide');
-		$('#form_fleetmember_action_cancel_'.concat(index)).removeClass('hide');
-		$('#form_fleetmember_action_cancel_edit_'.concat(index)).addClass('hide');
-	}
+function toggleFleetmemberEdit(index) {
+	$('div[member='+index+']').toggle();
 }
+
+$('a[data-toggle=collapse]').click(function() {
+	if ($(this).hasClass('active') === true) {
+		$(this).removeClass('active');
+	} else {
+		$(this).addClass('active');
+	}
+});
