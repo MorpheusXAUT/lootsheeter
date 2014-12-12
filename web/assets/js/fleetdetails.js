b7aa9f12a1f35b12f3c16ff559c32d9c400d3efd
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2014-12-12 05:18:00 +0100

    Moved displayError to script.js (included in all pages now)

--- web/assets/js/fleetdetails.js
+++ web/assets/js/fleetdetails.js
@@ -1,7 +1,3 @@
-function displayError(error) {
-	$('div.col-md').prepend('<div class="alert alert-danger alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><strong>Ooops!</strong> '+error+'</div>');
-}
-
 $(document).ready(function(e) {
 	$('a[data-toggle=collapse]').click(function() {
 		$(this).toggleClass('active');
