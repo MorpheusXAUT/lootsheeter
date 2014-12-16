8f32c1c087baff29582e66a789008031f61883c3
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2014-12-16 22:02:09 +0100

    Added scrolling to top of the page when displaying an error message

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,3 +1,4 @@
 function displayError(error) {
 	$('div.col-md').prepend('<div class="alert alert-danger alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><strong>Ooops!</strong> '+error+'</div>');
+	$('html, body').animate({ scrollTop: '0px' });
 }
