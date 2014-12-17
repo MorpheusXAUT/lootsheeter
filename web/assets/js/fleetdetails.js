f00cd3b8fad3c2571ab63600fd78277b9a266822
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2014-12-17 05:30:05 +0100

    Added search box to fleet details member adding, making it easier to find a new member in the list

--- web/assets/js/fleetdetails.js
+++ web/assets/js/fleetdetails.js
@@ -1,4 +1,8 @@
 $(document).ready(function(e) {
+	$(function() {
+		$('#addMemberSelectMember').filterByText($('#addMemberSelectMemberSearch'), true);
+	});
+	
 	$('a[data-toggle=collapse]').click(function() {
 		$(this).toggleClass('active');
 	});
--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -2,3 +2,30 @@ function displayError(error) {
 	$('div.col-md').prepend('<div class="alert alert-danger alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><strong>Ooops!</strong> '+error+'</div>');
 	$('html, body').animate({ scrollTop: '0px' });
 }
+
+jQuery.fn.filterByText = function(textbox, selectSingleMatch) {
+	return this.each(function() {
+		var select = this;
+		var options = [];
+		$(select).find('option').each(function() {
+			options.push({value: $(this).val(), text: $(this).text()});
+		});
+		$(select).data('options', options);
+		$(textbox).bind('change keyup', function() {
+			var options = $(select).empty().scrollTop(0).data('options');
+			var search = $.trim($(this).val());
+			var regex = new RegExp(search, "gi");
+			$.each(options, function(i) {
+				var option = options[i];
+				if (option.text.match(regex) !== null) {
+					$(select).append(
+						$('<option>').text(option.text).val(option.value)
+					);
+				}
+			});
+			if (selectSingleMatch === true && $(select).children().length() === 1) {
+				$(select).children().get(0).selected = true;
+			}
+		});
+	});
+};
