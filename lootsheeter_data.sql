e5c87492955c72822c365bfcae27e0b8c8bb1dec
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-02 14:02:07 +0100

    Added "use database" to lootsheeter_data.sql as a test for automatic database creation

--- lootsheeter_data.sql
+++ lootsheeter_data.sql
@@ -5,6 +5,8 @@
 -- HeidiSQL Version:             9.1.0.4882
 -- --------------------------------------------------------
 
+USE `lootsheeter`;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
